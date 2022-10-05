00185aac960f9c5861abefc71a88940934ac0187
Steerpike <imthekoul@gmail.com>
2022-10-05 17:37:16 +0300

    Modal Form

--- travel-website/src/Components/Navbar.tsx
+++ travel-website/src/Components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Navbar, Container, Nav, Button, Modal } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button, Modal, Form } from 'react-bootstrap';
 import LogoSrc from '../Assets/Images/logo.png';
 
 const Navbard = () => {
@@ -37,7 +37,43 @@ const Navbard = () => {
         <Modal.Header className=" bg-warning" closeButton>
           <Modal.Title>Book Your Holiday</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Take care of your bookings!</Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label aria-describedby="full-name">Full-name</Form.Label>
+              <Form.Control type="text" placeholder="Your Name" autoFocus />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+              <Form.Label aria-describedby="email">Email</Form.Label>
+              <Form.Control type="email" placeholder="email@email.com" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTex1">
+              <Form.Text>We will never share your email with anyone!</Form.Text>
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlSelect1"
+              aria-label="select-menu"
+            >
+              <Form.Select>
+                <option selected>Select your holiday package</option>
+                <option value="h1">Packet-1</option>
+                <option value="h2">Packet-2</option>
+                <option value="h3">Packet-3</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlSelect1"
+              aria-label="checkbox-menu"
+            >
+              <Form.Check
+                type="checkbox"
+                label="I agree with the terms and conditions"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="danger" onClick={handleClose}>
             Close
