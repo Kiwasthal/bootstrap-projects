8efdaffd1384830ef1ca72fd20fe4da1b3fca65b
Steerpike <imthekoul@gmail.com>
2022-10-05 14:59:13 +0300

    Testing bootstrap packages & imports

---
+++ travel-website/src/App.tsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
